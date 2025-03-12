22f9f887dd1333163745cd24a02d3b0819083f4b
Yunus Özkaya <92160164+YunusOzkaya@users.noreply.github.com>
2025-03-12 21:58:58 +0300

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -74,6 +74,16 @@ const galleryMarkup = images.map(({ preview, original, description }) => `
     </li>
 `).join("");
 
+const galleryEl = document.querySelector(".gallery");
+
+const galleryMarkup = images.map(({ preview, original, description }) => `
+    <li class="gallery-item">
+        <a class="gallery-link" href="${original}">
+            <img class="gallery-image" src="${preview}" data-source="${original}" alt="${description}" />
+        </a>
+    </li>
+`).join("");
+
 galleryEl.innerHTML = galleryMarkup;
 
 galleryEl.addEventListener("click", onGalleryClick);
